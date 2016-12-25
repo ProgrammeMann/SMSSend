337749ca0d89116c0646c6956c2dbc7c30772be6
programmemann <pavlov_egor007@mail.ru>
2016-12-25 11:55:14 +0300

    add comments with instructions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         SMS newSMS = new SMS();
-        newSMS.send_sms("79178587448", "Дз выполнено", 1, "", "", 0, "", "");
-        newSMS.get_sms_cost("79178587448", "Дз выполнено1", 0, 0, "", "");
+        newSMS.send_sms("79178587448", "Дз выполнено", 1, "", "", 0, "", "");//здесь вводим номер и сообщение
+        newSMS.get_sms_cost("79178587448", "Дз выполнено", 0, 0, "", "");
         newSMS.getBalance();
 
     }
